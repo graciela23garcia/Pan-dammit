bb31f66ab95fc7a60be7b8f8de6e07aafb9b9bc5
Dania Rinker <daniarinker@gmail.com>
2020-12-12 12:56:31 -0800

    exported routes to server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const expressHandlebars = require(`express-handlebars`);
 const PORT = process.env.PORT || 8080;
 
+//immediately invoked function express
+require(`./routes/routes.js`)(app, db.sequelize);
 //var db = require("./models");
 
 app.use(express.static(`public`));
