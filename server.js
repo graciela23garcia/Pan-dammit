cb293c13498b39a7ddbd71d7d553f0b08c833dc8
Dania Rinker <daniarinker@gmail.com>
2020-12-12 13:05:29 -0800

    added index.handlebars

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const expressHandlebars = require(`express-handlebars`);
 const PORT = process.env.PORT || 8080;
 
 //immediately invoked function express
-require(`./routes/routes.js`)(app, db.sequelize);
+// add in later ', db.sequelize'
+require(`./routes/routes.js`)(app);
 //var db = require("./models");
 
 app.use(express.static(`public`));
