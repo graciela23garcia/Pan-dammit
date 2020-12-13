d0f2807af21125fc889385da20cb96d3db4a31e5
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-12-12 16:10:57 -0800

    removed outdated import function unavailable in the most current version of sequalize, and replaced with require

--- models/index.js
+++ models/index.js
@@ -10,9 +10,9 @@ const db = {};
 let sequelize = null;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new SequelizeObj(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new SequelizeObj(config.database, config.username, config.password, config);
 }
 
 fs
@@ -21,7 +21,7 @@ fs
     return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
   })
   .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, SequelizeObj.DataTypes);
     db[model.name] = model;
   });
 
