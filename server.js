e6b3129a0fca903f1adbbd94a599c8c48e2a1405
Dania Rinker <daniarinker@gmail.com>
2020-12-16 17:36:35 -0800

    fixed eslint error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(session({ secret: `keyboard cat`, resave: true, saveUninitialized: true }));
 // Initializes passport
 app.use(passport.initialize());
 // Manages user data in session
