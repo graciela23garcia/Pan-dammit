063cad2753de770fc8f2354bdb039a231eca1223
Dania Rinker <daniarinker@gmail.com>
2020-12-16 17:14:12 -0800

    route editing

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,36 @@
-const express = require("express");
+'use strict';
 
-module.exports =
+module.exports = 
 (app, sequelize) => {
     app.get('/', (req, res) => {
-        // if (req.user) {
-            // res.redirect('');
-        // } else {
-            res.render('index');
+      if (req.user) {
+        res.redirect('/welcome');
+    } else {
+        res.render('login');
     });
-}
+
+    app.get('/login', (req, res) => {
+      if (req.user) {
+          res.redirect('/welcome');
+      } else {
+          res.render('login');
+  };
+
+    app.get('/welcome', isAuthenticated, (req, res) => {
+        res.render('welcomePage');
+    });
+
+
+
+    app.post("/api/signup", function(req, res) {
+        db.User.create({
+          email: req.body.email,
+          password: req.body.password
+        })
+          .then(function() {
+            res.redirect(307, "/api/login");
+          })
+          .catch(function(err) {
+            res.status(401).json(err);
+          });
+  }
