a95a8e15de82893f69607b180820b613ac6dd9df
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2020-12-18 20:25:18 -0800

    api & html routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,6 +22,43 @@ module.exports = function(app) {
         res.status(401).json(err);
       });
   });
+  app.post(`/api/blogPage`, (req, res) => {
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body
+    })
+      .then(() => {
+      // eslint-disable-next-line no-magic-numbers
+        res.status(200);
+      })
+      .catch(err => {
+      // eslint-disable-next-line no-magic-numbers
+        res.status(401).json(err);
+      });
+  });
+  app.get(`/api/blogPage`, (req, res) => {
+    db.Post.findAll({
+      order: [`post_time`, `DESC`]
+    })
+      .then(allBlogPosts => {
+      // eslint-disable-next-line no-magic-numbers
+        console.log(allBlogPosts);
+        res.render(`blogPage`, allBlogPosts);
+      })
+      .catch(err => {
+      // eslint-disable-next-line no-magic-numbers
+        res.status(401).json(err);
+      });
+  });
+  app.post(`/api/moviePage`, (req, res) => {
+    // eslint-disable-next-line no-magic-numbers
+    res.status(200);
+  });
+  app.get(`/api/moviePage`, (req, res) => {
+    // eslint-disable-next-line no-magic-numbers
+    res.status(200);
+  });
 
 };
+
 
