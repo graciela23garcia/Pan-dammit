e2d6226686b3ee46f0d5ccb4a82d4ace37cf0c36
Jaimie Valle <jsvalle@uw.edu>
2020-12-22 18:42:05 -0500

    using schema for blog post

--- public/js/blogPage.js
+++ public/js/blogPage.js
@@ -1,29 +1,40 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-magic-numbers */
 'use strict';
-const script = document.createElement(`script`);
-script.src = `https://code.jquery.com/jquery-3.4.1.min.js`;
-script.type = `text/javascript`;
-document.getElementsByTagName(`head`)[0].appendChild(script);
-console.log(`working!!!!`);
-$(document).ready(() => {
-  const submitBtnEl= $(`#submitBtn`);
-  const titleInputEl= $(`#blogTitle`);
-  const blogPostEl= $(`#blogPost`);
 
-  submitBtnEl.on(`click`, () => {
-    const blogPost = {
-      title: titleInputEl.val().trim(),
-      body: blogPostEl.val().trim()
-    };
-    if (!blogPost.title || !blogPost.body){
-      return;
+modeule.exports = function(sequelize, DataTypes) {
+  const Post = sequelize.define(`Post`, {
+    title : {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.Text,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    post_time: {
+      type: DataTypes.DATE,
+      allowNull: BooleanConstructor,
+      validate: {
+        len: [1]
+      }
+    },
+    UserID: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+
     }
-    console.log(blogPost);
-    $.post(`/api/blogPage`, blogPost).then(() => {
-      console.log(`success blogpost sent to backend ${blogPost}`);
-    } ).catch(err => {
-      console.log(err.responseJSON);
-    });
-titleInputEl.val(``);
-blogPostEl.val(``);
   });
-});
+  return Post;
+
+};
+
+document.getElementById(`Postbtn`).addEventListener(`click`, displayPost);
+
+
