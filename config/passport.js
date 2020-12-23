c854e88514248bff717306bd06dcf90ec76c7d1b
Kristena Arushanov <kris.arushanov@gmail.com>
2020-12-22 18:20:35 -0800

    updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,49 +1,48 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+'use strict';
+const LocalStrategy = require(`passport-local`).Strategy;
 
-var db = require("../models");
+// Load User Model
+const User = require(`../models/user`);
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
-passport.use(new LocalStrategy(
-  // Our user will sign in using username
-  {
-    usernameField: "username"
-  },
-  function(username, password, done) {
-    // When a user tries to sign in this code runs
-    db.User.findOne({
-      where: {
-        username: username
-      }
-    }).then(function(dbUser) {
-      // If there's no user with the given email
-      if (!dbUser) {
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField:`Users`},
+      username => {
+
+        // When a user tries to sign in this code runs
+        User.findOne({
+          username
+        }
+        ); }).then(user => {
+      if (!user) {
         return done(null, false, {
-          message: "Incorrect username ."
+          message: `Incorrect username`
         });
       }
-      // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+
+      else if (!user.validPassword(password)) {
         return done(null, false, {
-          message: "Incorrect password."
+          message: `Incorrect password.`
         });
       }
       // If none of the above, return the user
-      return done(null, dbUser);
-    });
-  }
-));
+      return done(null, User);
+    }
+    ));
 
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, cb) {
-  cb(null, users);
-});
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+  // In order to help keep authentication state across HTTP requests,
+  // Sequelize needs to serialize and deserialize the user
+  // Just consider this part boilerplate needed to make it all work
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
 
-// Exporting our configured passport
-module.exports = passport;
+  passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
+  });
+
+
+  // Exporting our configured passport
+  module.exports = passport;
+};
