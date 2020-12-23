0dd8a82fc8aaa5acea69e5e913cc1bc6a4c1663b
Jaimie Valle <jsvalle@uw.edu>
2020-12-22 21:13:07 -0500

    created the blog post POST and GET

--- public/js/blogPage.js
+++ public/js/blogPage.js
@@ -1,40 +1,72 @@
+/* eslint-disable max-nested-callbacks */
 /* eslint-disable camelcase */
 /* eslint-disable no-magic-numbers */
 'use strict';
 
-module.exports = function(sequelize, DataTypes) {
-  const Post = sequelize.define(`Post`, {
-    title : {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    post_time: {
-      type: DataTypes.DATE,
-      allowNull: BooleanConstructor,
-      validate: {
-        len: [1]
-      }
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
 
+const script = document.createElement(`script`);
+script.src = `https://code.jquery.com/jquery-3.4.1.min.js`;
+script.type = `text/javascript`;
+document.getElementsByTagName(`head`)[0].appendChild(script);
+console.log(`working!!!!`);
+$(document).ready(() => {
+  const submitBtnEl= $(`#submitBtn`);
+  const titleInputEl= $(`#blogTitle`);
+  const blogPostEl= $(`#blogPost`);
+  const postAreaEl= $(`#postArea`);
+  $.get(`/api/blogPage`, {
+    method: `GET`,
+    headers: { 'Content-Type': `application/json` }
+  })
+    .then(res => {
+      console.log(res);
+      for(let i = 0 ; i < res.length; i++){
+        const titleEl= $(`<h4>`);
+        titleEl.text(res[i].title);
+        const bodyEl = $(`<p>`);
+        bodyEl.text(res[i].body);
+        postAreaEl.append(titleEl);
+        postAreaEl.append(bodyEl);
+      }
+
+    })
+    .catch(err => console.log(err));
+  submitBtnEl.on(`click`, () => {
+    const blogPost = {
+      title: titleInputEl.val().trim(),
+      body: blogPostEl.val().trim()
+    };
+    if (!blogPost.title || !blogPost.body){
+      return;
     }
+    console.log(blogPost);
+    $.post(`/api/blogPage`, blogPost).then(() => {
+      console.log(`success blogpost sent to backend ${blogPost}`);
+      $.get(`/api/blogPage`, {
+        method: `GET`,
+        headers: { 'Content-Type': `application/json` }
+      })
+        .then(res => {
+          console.log(res);
+          for(let i = 0 ; i < res.length; i++){
+            const titleEl= $(`<h4>`);
+            titleEl.text(res[i].title);
+            const bodyEl = $(`<p>`);
+            bodyEl.text(res[i].body);
+            postAreaEl.append(titleEl);
+            postAreaEl.append(bodyEl);
+          }
+
+        })
+        .catch(err => console.log(err));
+    } ).catch(err => {
+      console.log(err.responseJSON);
+    });
+    titleInputEl.val(``);
+    blogPostEl.val(``);
+
+
   });
-  return Post;
-
-};
-
-document.getElementById(`Postbtn`).addEventListener(`click`, displayPost);
+});
 
 
